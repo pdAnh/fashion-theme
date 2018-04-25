3b587e502f2b012d5ec0d1746706fcb4521ca168
pdAnh <pdanh012096@gmail.com>
2018-04-25 10:53:35 +0700

    done page index

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const compression = require('compression')
+const bodyParser = require('body-parser')
+
+const server = express()
+
+server.set('port', process.env.PORT || 3020)
+server.use(express.static('public'))
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+
+server.get("/product", (req, res) => {
+  res.sendFile(__dirname + "/public/product.html")
+})
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html")
+})
+server.get("/category", (req, res) => {
+  res.sendFile(__dirname + "/public/category.html")
+})
+
+server.listen(server.get('port'), (req, res) => {
+  console.log("Server is running on port %s...", server.get('port'));
+})
